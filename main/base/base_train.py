b706132d6e6eec176a44b046f133ca3ff4d3f369
park3hho <ejfhr7777@gmail.com>
2025-12-22 22:05:39 +0900

    0.0.13 llm_fft: train
    (1) -------
    (2) -------
    (3)

---
+++ main/base/base_train.py
@@ -0,0 +1,36 @@
+import torch
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+#device = "cpu"
+torch.manual_seed(123)
+model.to(device)
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.00001, weight_decay=0.01)
+
+tokens_seen, global_step = 0, -1
+
+losses = []
+
+for epoch in range(10):
+    model.train()  # Set model to training mode
+
+    epoch_loss = 0
+    for input_batch, target_batch in train_loader:
+        optimizer.zero_grad()  # Reset loss gradients from previous batch iteration
+        input_batch, target_batch = input_batch.to(device), target_batch.to(device)
+
+        logits = model(input_batch).logits  # 뒤에 .logits를 붙여서 tensor만 가져옴
+
+        loss = torch.nn.functional.cross_entropy(logits.flatten(0, 1), target_batch.flatten())
+        epoch_loss += loss.item()
+        loss.backward()  # Calculate loss gradients
+        optimizer.step()  # Update model weights using loss gradients
+        tokens_seen += input_batch.numel()
+        global_step += 1
+
+        print(f"{global_step} Tokens seen: {tokens_seen}")
+
+    avg_loss = epoch_loss / len(train_loader)
+    losses.append(avg_loss)
+    print(f"Epoch: {epoch}, Loss: {avg_loss}")
+    torch.save(model.state_dict(), "model_" + str(epoch).zfill(3) + ".pth")
